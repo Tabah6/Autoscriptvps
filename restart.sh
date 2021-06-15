0616ab20741e17c6547fe7ce48b92ea1b71b2bac
Endka22 <74788605+Endka22@users.noreply.github.com>
2021-06-16 01:14:59 +0700

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -43,6 +43,7 @@ case $Restart in
                 /etc/init.d/cron restart
                 /etc/init.d/nginx restart
                 /etc/init.d/squid restart
+                /etc/init.d/v2ray restart
                 screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7100 --max-clients 1000
                 screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7200 --max-clients 1000
                 screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 1000
