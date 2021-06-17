c5dc9110edd028466a6c21d41a37863b99f4539b
Endka22 <74788605+Endka22@users.noreply.github.com>
2021-06-18 01:07:34 +0700

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -50,12 +50,16 @@ url=$(rclone link dr:backup/$IP-$date.zip)
 id=(`echo $url | grep '^https' | cut -d'=' -f2`)
 link="https://drive.google.com/u/4/uc?id=${id}&export=download"
 echo -e "The following is a link to your vps data backup file.
+
 ===================
 Your VPS IP $IP
 Domain=${domain}
 ===================
+
 $link
+
 If you want to restore data, please enter the link above.
+
 Thank You For Using Our Services.
 @Copiright 2021 By Endka" | mail -s "Backup Data" $email
 rm -rf /root/backup
